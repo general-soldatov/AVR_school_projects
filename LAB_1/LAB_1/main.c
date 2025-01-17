173f7b3d2019b00087c8cf9e0f6db36e20d1fa6c
general-soldatov <general-soldatov@mail.ru>
2025-01-17 19:32:21 +0300

    initialize repo

---
+++ LAB_1/LAB_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * LAB_1.c
+ *
+ * Created: 17.01.2025 19:29:05
+ * Author : GS
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
